af3685280844a6e17df6a279ca3259d04f096f0b
csandoval <chrissandoval916@gmail.com>
2024-03-20 09:25:21 -0700

    Too strict on check

--- resources/views/livewire/entry/view.blade.php
+++ resources/views/livewire/entry/view.blade.php
@@ -10,11 +10,11 @@
             <li>City: {{ $entry->city }}</li>
             <li>Country: {{ $entry->country }}</li>
             <li>Birthdate: {{ $entry->birthdate }}</li>
-            <li>Is Married: {{ $entry->is_married === 1 ? 'Yes' : 'No' }}</li>
+            <li>Is Married: {{ $entry->is_married ? 'Yes' : 'No' }}</li>
             <li>Marriage Date: {{ $entry->marriage_date }}</li>
             <li>Marriage Country: {{ $entry->marriage_country }}</li>
-            <li>Is a Widow: {{ $entry->is_widowed === 1 ? 'Yes' : 'No'  }}</li>
-            <li>Is Separated: {{ $entry->is_separated === 1 ? 'Yes' : 'No'  }}</li>
+            <li>Is a Widow: {{ $entry->is_widowed ? 'Yes' : 'No'  }}</li>
+            <li>Is Separated: {{ $entry->is_separated ? 'Yes' : 'No'  }}</li>
         </ul>
     </div>
 </div>
